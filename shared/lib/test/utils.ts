605621ce09f9efc5736a35500867dda4f2b0164b
dongtimes2 <dongtimes2@gmail.com>
2024-12-03 03:01:20 +0900

    fix: fix custom test render

--- shared/lib/test/utils.ts
+++ shared/lib/test/utils.ts
@@ -2,7 +2,7 @@ import { render, RenderOptions } from "@testing-library/react";
 
 import { Providers } from "@/app/providers";
 
-const customRender = (ui: React.ReactNode, options: RenderOptions) => render(ui, { wrapper: Providers, ...options });
+const customRender = (ui: React.ReactNode, options?: RenderOptions) => render(ui, { wrapper: Providers, ...options });
 
 export { customRender as render };
 export { screen } from "@testing-library/react";
